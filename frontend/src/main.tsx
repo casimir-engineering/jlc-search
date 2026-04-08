99a171672f11aa379346f488c82a6dc64ff540d0
Julien Buteau <julien.buteau@gmail.com>
2026-04-08 10:55:01 +0200

    feat: add PostHog analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
 import { App } from "./App.tsx";
 import "./styles/index.css";
+
+posthog.init("phc_mATHaDwUWBQvdHFsATWxKYFsfjBfZKyx9cLJEmpF9oDY", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+  capture_pageview: true,
+  capture_pageleave: true,
+  autocapture: true,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
